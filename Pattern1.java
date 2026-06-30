f909b094bf4b9dfc6dd3eca5d36319b8a1c181c5
Prasath <p93554926@gmail.com>
2026-06-30 21:44:12 +0530

    Add files via upload

---
+++ Pattern1.java
@@ -0,0 +1,21 @@
+package Pattern;
+
+public class Pattern1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		 int n = 5;
+
+	        for (int i = n; i >= 1; i--) {
+	            for (int j = 1; j <= n - i; j++) {
+	                System.out.print(" ");
+	            }
+	            for (int k = 1; k <= (2 * i - 1); k++) {
+	                System.out.print("*");
+	            }
+	            System.out.println();
+	        }
+	}
+
+}
