57c7b7042ea7e9c9de95e9005e6565dda7db6165
Prasath <p93554926@gmail.com>
2026-06-30 21:44:49 +0530

    Add files via upload

--- Invertedpyramidstarpattern.java
+++ Invertedpyramidstarpattern.java
@@ -1,20 +1,19 @@
 package Sample;
 
-public class Pattern1 {
+public class Invertedpyramidstarpattern {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		int n=5;
-        for(int i=n;i>=1;i--) {
-            for(int j=1;j<=n-i;j++) {
+        for (int i=n;i>=1;i--) {
+            for (int j=0;j<n-i;j++) {
                 System.out.print(" ");
             }
-            for(int k=1;k<=(2*i-1);k++) {
-                System.out.print("*");
+            for (int k=1;k<=i;k++) {
+                System.out.print("* ");
             }
             System.out.println();
         }
 	}
-
 }
